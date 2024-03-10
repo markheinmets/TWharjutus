6230dadf3ff1e77cbaa5e00940f14ed385a4a7fa
markheinmets <markheinmets97@gmail.com>
2024-03-10 21:53:36 +0200

    1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      myColor: "#4CAF4F",
+      gray: "#717171",
+      white: "#FFFFFF",
+      dgray: "#4D4D4D",
+      silver: "#F5F7FA",
+
+    },
+  },
+  plugins: [],
+};
